244355ea49aaa9d33170229d05cb6bb0acf94b52
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-04-18 13:39:39 +0900

    learnReduxutility

--- src/store/actions/counter.js
+++ src/store/actions/counter.js
@@ -22,3 +22,7 @@ export const substoract = (value) =>{
         val:value
     }
 }
+
+
+
+
--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -1,7 +1,6 @@
 import * as actionTypes from './actionTypes'
 
 export const saveResult = (res) =>{
-
     return {
         type:actionTypes.STORE_RESULT,
         result:res
